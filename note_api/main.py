fc0b470013a425615b2b35f8f984518171e3afab
TwumasiAckah <118646126+TwumasiAckah@users.noreply.github.com>
2025-01-17 12:16:38 +0100

    update OT3

--- note_api/main.py
+++ note_api/main.py
@@ -9,8 +9,26 @@ from starlette.responses import RedirectResponse
 from .backends import Backend, RedisBackend, MemoryBackend, GCSBackend
 from .model import Note, CreateNoteRequest
 
-# from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry import trace, baggage
+from opentelemetry.trace import SpanContext, NonRecordingSpan
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+from opentelemetry.propagate import set_global_textmap, inject, extract
+from opentelemetry.propagators.cloud_trace_propagator import (
+    CloudTraceFormatPropagator,
+)
+set_global_textmap(CloudTraceFormatPropagator())
+
+
+tracer_provider = TracerProvider()
+cloud_trace_exporter = CloudTraceSpanExporter()
+tracer_provider.add_span_processor(
+    BatchSpanProcessor(cloud_trace_exporter)
+)
+trace.set_tracer_provider(tracer_provider)
+tracer = trace.get_tracer(__name__)
 
 app = FastAPI()
 
@@ -39,14 +57,26 @@ def redirect_to_notes() -> None:
     return RedirectResponse(url='/notes')
 
 
+# @app.get('/notes')
+# def get_notes(backend: Annotated[Backend, Depends(get_backend)]) -> List[Note]:
+#     keys = backend.keys()
+
+#     Notes = []
+#     for key in keys:
+#         Notes.append(backend.get(key))
+#     return Notes
+
+
 @app.get('/notes')
 def get_notes(backend: Annotated[Backend, Depends(get_backend)]) -> List[Note]:
-    keys = backend.keys()
-
-    Notes = []
-    for key in keys:
-        Notes.append(backend.get(key))
-    return Notes
+    """Fetch all notes from the backend"""
+    with tracer.start_as_current_span("get_notes") as span:
+        span.set_attribute("backend.type", type(backend).__name__)
+        keys = backend.keys()
+        notes = []
+        for key in keys:
+            notes.append(backend.get(key))
+        return notes
 
 
 @app.get('/notes/{note_id}')
