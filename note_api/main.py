10a09ab58582dc9ae1996ac04a2b972b3a71de3f
TwumasiAckah <118646126+TwumasiAckah@users.noreply.github.com>
2025-01-17 06:52:29 +0100

    remove OT

--- note_api/main.py
+++ note_api/main.py
@@ -9,47 +9,10 @@ from starlette.responses import RedirectResponse
 from .backends import Backend, RedisBackend, MemoryBackend, GCSBackend
 from .model import Note, CreateNoteRequest
 
-import os
-from opentelemetry import trace, metrics
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.trace import OTLPSpanExporter
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.exporter.otlp.metrics import OTLPMetricExporter
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-
-
-# # Setup Resource with custom attributes (e.g., service instance ID)
-# resource = Resource.create(attributes={
-#     "service.instance.id": f"worker-{os.getpid()}",  # Unique service instance ID based on the worker process ID
-# })
-
-# # Set up TracerProvider with the resource
-# traceProvider = TracerProvider(resource=resource)
-
-# # Set up the BatchSpanProcessor with the OTLP exporter
-# processor = BatchSpanProcessor(OTLPSpanExporter())
-# traceProvider.add_span_processor(processor)
-
-# # Set the TracerProvider to OpenTelemetry's global trace provider
-# trace.set_tracer_provider(traceProvider)
-
-# # Set up the PeriodicExportingMetricReader for metrics with OTLP exporter
-# reader = PeriodicExportingMetricReader(OTLPMetricExporter())
-# meterProvider = MeterProvider(metric_readers=[reader], resource=resource)
-
-# # Set the MeterProvider for metrics collection
-# metrics.set_meter_provider(meterProvider)
-
-
 app = FastAPI()
 
 my_backend: Optional[Backend] = None
 
-# # Instrument the FastAPI app for tracing
-# FastAPIInstrumentor.instrument_app(app)
 
 def get_backend() -> Backend:
     global my_backend  # pylint: disable=global-statement
